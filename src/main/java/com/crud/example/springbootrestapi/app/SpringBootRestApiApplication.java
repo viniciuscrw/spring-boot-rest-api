361e2a6fbe21344bc4d527be5452b565ad7ad4fd
Vinicius Costa <viniciuscr@ciandt.com>
2019-10-26 11:14:36 -0300

    Initial commit

---
+++ src/main/java/com/crud/example/springbootrestapi/app/SpringBootRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.crud.example.springbootrestapi.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiApplication.class, args);
+	}
+
+}
